3402ad3453d0e586e644108cf41dd0c0cff1edf0
Vanessa9914 <vanessagonzalez9914@gmail.com>
2026-04-11 15:33:49 -0500

    Agregar tabla usuarios

--- sql/donal_store.sql
+++ sql/donal_store.sql
@@ -8,6 +8,16 @@ CREATE DATABASE donal_store;
 USE donal_store;
 
 -- =========================
+-- TABLA usuarios
+-- =========================
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- =========================
 -- TABLA productos
 -- =========================
 CREATE TABLE productos (
