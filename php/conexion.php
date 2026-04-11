dc65f621e1da808fbfdd797752c8c55124ee7b67
Troleadorsans <xxtroleadorsansxx@gmail.com>
2026-04-11 14:08:21 -0500

    prueba conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,3 +1,17 @@
 <?php
-gtbtbtb
+ function ConexionBaseDatos(){
+ $host="localhost";
+ $base_datos="donal_store";
+    $usuario="root";
+$contraseña="";
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$base_datos", $usuario, $contraseña);
+ $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ return "Conexion exitosa :D";
+ }catch(PDOException $e){
+ return "Error en la conexion :(".$e->getMessage();
+ }
+
+ }
+ echo ConexionBaseDatos(); 
 ?>
