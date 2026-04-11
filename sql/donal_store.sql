a88d9fedc63fe2386b4efe1c7cf72ade97c00737
Vanessa9914 <vanessagonzalez9914@gmail.com>
2026-04-11 15:07:55 -0500

    correción sintaxis  sql

--- sql/donal_store.sql
+++ sql/donal_store.sql
@@ -7,9 +7,19 @@ CREATE DATABASE donal_store;
 -- Usar base de datos
 USE donal_store;
 
+-- =========================
+-- TABLA usuarios
+-- =========================
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
 
---TABLA productos
-
+-- =========================
+-- TABLA productos
+-- =========================
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(150) NOT NULL,
@@ -19,40 +29,46 @@ CREATE TABLE productos (
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
---TABLE: pedidos
---=====================
+-- =========================
+-- TABLA pedidos
+-- =========================
 CREATE TABLE pedidos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     total DECIMAL(10,2),
-    FOREING KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
---TABLE: detalle pedido
---======================
-
+-- =========================
+-- TABLA detalle_pedido
+-- =========================
 CREATE TABLE detalle_pedido (
     id INT AUTO_INCREMENT PRIMARY KEY,
     pedido_id INT,
     producto_id INT,
     cantidad INT,
     precio_unitario DECIMAL(10,2),
-    FOREING KEY (pedido_id) REFERENCES pedidos(id)
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
 );
 
---Usuarios
+-- =========================
+-- DATOS DE usuarios
+-- =========================
 INSERT INTO usuarios (nombre, email, password)
 VALUES
 ('Vanessa', 'vanessa@gmail.com', '123456'),
 ('Carlos', 'carlos@gmail.com', '123456');
 
---Productos
+-- =========================
+-- DATOS DE productos
+-- =========================
 INSERT INTO productos (nombre, descripcion, precio, stock)
 VALUES
 ('Blusa básica', 'Blusa elegante color beige', 80000, 60),
 ('Blusa manga corta', 'Blusa manga corta negra', 120000, 100),
 ('Vestido corto', 'Vestido casual para verano', 180000, 90),
 ('Vestido largo', 'Vestido largo elegante', 230000, 76),
-('Short', 'short casual para verano', 180000, 90),
-('Short corto', 'Short ', 180000, 90),
+('Short', 'Short casual para verano', 180000, 90),
+('Short corto', 'Short corto casual', 180000, 90);
